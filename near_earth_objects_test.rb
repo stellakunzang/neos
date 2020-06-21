b70e9599a28c0f46491ebb81eea327de2a7eeb35
stellakunzang <stella.k.bonnie@gmail.com>
2020-06-21 17:22:23 -0600

    Add more tests to help guide refactor

--- near_earth_objects_test.rb
+++ near_earth_objects_test.rb
@@ -4,8 +4,34 @@ require 'pry'
 require_relative 'near_earth_objects'
 
 class NearEarthObjectsTest < Minitest::Test
+
+  def test_it_exists
+    neos = NearEarthObjects.new('2019-03-30')
+    assert_instance_of NearEarthObjects, neos
+  end
+
+  def test_it_has_attributes
+    neos = NearEarthObjects.new('2019-03-30')
+    assert_equal "2019-03-30", neos.date 
+  end
+
+  def test_parsed_asteroids_data_is_array
+    neos = NearEarthObjects.new('2019-03-30')
+    assert_equal Array, neos.parsed_asteroids_data.class
+  end
+
   def test_a_date_returns_a_list_of_neos
     results = NearEarthObjects.find_neos_by_date('2019-03-30')
-    assert_equal '(2019 GD4)', results[:astroid_list][0][:name]
+    assert_equal '(2019 GD4)', results[:asteroid_list][0][:name]
+  end
+
+  def test_neos_hash
+    results = NearEarthObjects.find_neos_by_date('2019-03-30')
+    assert_equal Hash, results.class
+    assert_equal 3, results.length
+    assert_equal Array, results[:asteroid_list].class
+    assert_equal Hash, results[:asteroid_list][0].class
+    assert_equal Integer, results[:biggest_asteroid].class
+    assert_equal Integer, results[:total_number_of_asteroids].class
   end
 end
